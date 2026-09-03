bc436c7deb5b36a987d730bf8c7a6788627fd76c
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-03 18:59:52 +0200

    feat(m11): export adaptive routing API

---
+++ src/routing/index.ts
@@ -0,0 +1,43 @@
+export { BoundedRoutingResultCache } from "./cache.js";
+export {
+  createEvaluationRecord,
+  evaluationContentHash,
+  ModelEvaluationRegistry,
+  normalizeEvaluation,
+} from "./evaluations.js";
+export {
+  evaluateRoutingFixture,
+  type RoutingEvaluationCase,
+  type RoutingEvaluationOutcome,
+  type RoutingEvaluationSummary,
+} from "./eval-harness.js";
+export { AdaptiveReasoningController, createReasoningPlan } from "./reasoning.js";
+export { EmpiricalModelRouter, normalizeRouteRequest } from "./router.js";
+export type {
+  CachedRoutingResult,
+  EmpiricalRouterConfig,
+  EvaluationProducer,
+  EvaluationProducerClass,
+  EvidenceVerdict,
+  ModelEvaluation,
+  ModelEvaluationInput,
+  ReasoningAttemptState,
+  ReasoningBranch,
+  ReasoningBranchKind,
+  ReasoningControllerDecision,
+  ReasoningEvidenceSignal,
+  ReasoningNextAction,
+  ReasoningPlan,
+  RouteRequest,
+  RoutingBudget,
+  RoutingCacheDecision,
+  RoutingCacheRequest,
+  RoutingCandidate,
+  RoutingDecision,
+  RoutingErrorCode,
+  RoutingInputs,
+  RoutingRequirements,
+  RoutingRisk,
+  RoutingTaskClass,
+} from "./types.js";
+export { RoutingError } from "./types.js";
